838594fa861298a5351ed67580ad37776059c478
WashU-Yifan <wang.y1@wustl.edu>
2022-10-01 03:57:57 +0800

    "step1"

---
+++ Studio4/studio4.cpp
@@ -0,0 +1,12 @@
+#include "studio4.h"
+using namespace std;
+const int SUCCESS = 0;
+const int days=7;
+const int num_pressure=3;
+
+int main(int argc, char * argv[]){
+    const char* c1="Hello, ",c2="World!";
+    cout<<"\""<<c1<<"\""<< ", length: "<<strlen(c1)<<endl;
+    cout<<"\""<<c2<<"\""<< ", length: "<<strlen(c2)<<endl;
+    return SUCCESS;
+}
