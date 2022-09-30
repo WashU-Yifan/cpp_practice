cc95c6abb4b4ca70dcbba96f0a3efa2e1aeaa49c
WashU-Yifan <wang.y1@wustl.edu>
2022-09-30 15:02:03 -0500

    step 1done

--- Studio4/studio4.cpp
+++ Studio4/studio4.cpp
@@ -5,7 +5,7 @@ const int days=7;
 const int num_pressure=3;
 
 int main(int argc, char * argv[]){
-    const char* c1="Hello, ",c2="World!";
+    const char* c1="Hello, ",*c2="World!";
     cout<<"\""<<c1<<"\""<< ", length: "<<strlen(c1)<<endl;
     cout<<"\""<<c2<<"\""<< ", length: "<<strlen(c2)<<endl;
     return SUCCESS;
