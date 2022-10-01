995fe019dbfffa1e3fabd5d5b100e5482ddba2a4
WashU-Yifan <wang.y1@wustl.edu>
2022-10-01 10:47:41 +0800

    "step 3"

--- Studio4/studio4.cpp
+++ Studio4/studio4.cpp
@@ -6,7 +6,9 @@ const int num_pressure=3;
 
 int main(int argc, char * argv[]){
     const char* c1="Hello, ",*c2="World!";
-    cout<<"\""<<c1<<"\""<< ", length: "<<strlen(c1)<<endl;
-    cout<<"\""<<c2<<"\""<< ", length: "<<strlen(c2)<<endl;
+    size_t (*p)(const char * );
+    p=strlen;
+    cout<<"\""<<c1<<"\""<< ", length: "<<p(c1)<<endl;
+    cout<<"\""<<c2<<"\""<< ", length: "<<p(c2)<<endl;
     return SUCCESS;
 }
