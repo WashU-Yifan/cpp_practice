50a09542ad787bc9e45010582d6046f1fbae4197
WashU-Yifan <wang.y1@wustl.edu>
2022-10-01 11:27:53 +0800

    "studio4"

--- Studio4/studio4.cpp
+++ Studio4/studio4.cpp
@@ -10,5 +10,14 @@ int main(int argc, char * argv[]){
     p=strlen;
     cout<<"\""<<c1<<"\""<< ", length: "<<p(c1)<<endl;
     cout<<"\""<<c2<<"\""<< ", length: "<<p(c2)<<endl;
+    float f=0.0;
+    cout<<"f is :"<<f<<endl;
+    cout<<"sin(cos(f)) is:" <<sin(cos(f))<<endl;
+    function<float(float)> sn =[](auto i){return sin(i);};
+    function<float(float)> cs =[](auto i){return cos(i);};
+    cout<<sn(cs(i))<<endl;
+    auto compose_result=compose(sn,cs);
+    cout<<compose_result(i)<<endl;
+    cout<<bind(compose_result,i)<<endl;
     return SUCCESS;
 }
--- Studio4/studio4.h
+++ Studio4/studio4.h
@@ -2,3 +2,6 @@
 #include <cstring>
 #include <algorithm>
 #include <iterator>
+#include<cmath>
+#include<functional>
+#include"Compose_T.h"
