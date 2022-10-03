3ca218a5ca8ce44dbf619130aba0ee9e0d37b798
Yifan Wang <68475921+WashU-Yifan@users.noreply.github.com>
2022-10-03 23:24:23 +0800

    "second"

--- Studio5/s5.cpp
+++ Studio5/s5.cpp
@@ -3,6 +3,10 @@
 using namespace std;
 
 int main(){
-    point2D p1;//=point2D();
+    point2D1 p1;//=point2D();
     cout<<p1.x<<p1.y<<endl;
+    point2D2 p2;//=point2D();
+    cout<<p2.x<<p2.y<<endl;
+    point2D3 p3;//=point2D();
+    cout<<p3.x<<p3.y<<endl;
 }
--- Studio5/s5.h
+++ Studio5/s5.h
@@ -1,6 +1,19 @@
 #include<iostream>
 
-struct point2D{
-    int x,y;
-    point2D():x(0),y(0){}
+class point2D1{
+        point2D():x(0),y(0){}
+    private:
+        int x,y;
+
+};
+class point2D2{
+      point2D():x(0),y(0){}
+    protected:
+        int x,y;
+  
+};
+class point2D3{
+    public:
+        int x,y;
+        point2D():x(0),y(0){}
 };
