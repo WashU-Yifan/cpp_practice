e375e6b2cfb0c172fe038895ce6ab96a6d29ffab
Yifan Wang <68475921+WashU-Yifan@users.noreply.github.com>
2022-10-03 23:42:52 +0800

    "4"

--- Studio5/s5.h
+++ Studio5/s5.h
@@ -1,14 +1,15 @@
 #include<iostream>
 
 class point2D1{
+    private:
+        int x,y;
     public:
-        point2D():x(0),y(0){}
+        point2D1():x(0),y(0){}
         int x()const{return x;}
         int y()const{ return y;}
         point2D1& set_x(int _x){x=_x; return *this;}
         point2D1& set_y(int _y){y=_y; return *this;}
 
-    private:
-        int x,y;
+
 
 };
