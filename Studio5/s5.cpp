7c50fcf6b7c57efbb5a467b4571fa41cbe674685
Yifan Wang <68475921+WashU-Yifan@users.noreply.github.com>
2022-10-03 23:41:28 +0800

    "4"

--- Studio5/s5.cpp
+++ Studio5/s5.cpp
@@ -3,10 +3,10 @@
 using namespace std;
 
 int main(){
-    point2D1 p1;//=point2D();
-    cout<<p1.x<<p1.y<<endl;
-    point2D2 p2;//=point2D();
-    cout<<p2.x<<p2.y<<endl;
-    point2D3 p3;//=point2D();
-    cout<<p3.x<<p3.y<<endl;
+    point2D1 p1;
+    cout<<"access on nonconst: "<<p1.x()<<endl;
+    cout<<"mutate then access on nonconst: "<<p1.set_x(1).x()<<endl;
+   const point2D1 p2;
+    cout<<"access on nonconst: "<<p2.x()<<endl;
+    //cout<<"mutate then access on const: "<<p2.set_x(1).x()<<endl;
 }
--- Studio5/s5.h
+++ Studio5/s5.h
@@ -1,19 +1,14 @@
 #include<iostream>
 
 class point2D1{
+    public:
         point2D():x(0),y(0){}
+        int x()const{return x;}
+        int y()const{ return y;}
+        point2D1& set_x(int _x){x=_x; return *this;}
+        point2D1& set_y(int _y){y=_y; return *this;}
+
     private:
         int x,y;
 
-};
-class point2D2{
-      point2D():x(0),y(0){}
-    protected:
-        int x,y;
-  
-};
-class point2D3{
-    public:
-        int x,y;
-        point2D():x(0),y(0){}
 };
