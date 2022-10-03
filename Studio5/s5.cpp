cb4dfc04cfcb9ec77f5d090948fb133f44fad3d8
WashU-Yifan <wang.y1@wustl.edu>
2022-10-03 10:47:32 -0500

    4

--- Studio5/s5.cpp
+++ Studio5/s5.cpp
@@ -4,9 +4,9 @@ using namespace std;
 
 int main(){
     point2D1 p1;
-    cout<<"access on nonconst: "<<p1.x()<<endl;
-    cout<<"mutate then access on nonconst: "<<p1.set_x(1).x()<<endl;
+    cout<<"access on nonconst: "<<p1.get_x()<<endl;
+    cout<<"mutate then access on nonconst: "<<p1.set_x(1).get_x()<<endl;
    const point2D1 p2;
-    cout<<"access on nonconst: "<<p2.x()<<endl;
+    cout<<"access on nonconst: "<<p2.get_x()<<endl;
     //cout<<"mutate then access on const: "<<p2.set_x(1).x()<<endl;
 }
