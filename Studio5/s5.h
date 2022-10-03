707147a078e63e0d2b9acdc3df57e56482517c52
Yifan Wang <68475921+WashU-Yifan@users.noreply.github.com>
2022-10-04 00:11:52 +0800

    "7"

--- Studio5/s5.h
+++ Studio5/s5.h
@@ -4,8 +4,15 @@ class point2D1{
     private:
         int x,y;
     public:
-        point2D1():x(0),y(0){}
-        point2D1(const point2D1& p2):x(p2.get_x()),y(p2.get_y()){}
+        point2D1():x(0),y(0){
+            std::cout<<"calling default constructor"<<std::endl;
+            std::cout<<"address"<< this<<std::endl;
+            std::cout<<"variables value:"<<x<<y<<std::endl;
+        }
+        point2D1(const point2D1& p2):x(p2.x),y(p2.y){
+            std::cout<<"calling copy constructor"<<std::endl;
+            std::cout<<"address"<< this<<std::endl;
+            std::cout<<"variables value:"<<x<<y<<std::endl;}
         int get_x()const{return x;}
         int get_y()const{ return y;}
         point2D1& set_x(int _x){x=_x; return *this;}
@@ -14,3 +21,4 @@ class point2D1{
 
 
 };
+point2D1 take(point2D1 p1){return p1;}
