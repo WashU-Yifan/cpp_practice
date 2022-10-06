d6a6ab1d1c88032e2533542fd73c000c6e5d7603
WashU-Yifan <wang.y1@wustl.edu>
2022-10-07 00:32:11 +0800

    "1"

---
+++ 6/6.h
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <set>
+#include <algorithm>
+#include <iterator>
+enum class Weekday{
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday,
+    Sunday
+    
+};
+
+enum class Pressure{
+    lo,
+    med,
+    hi,
+    pop
+};
+
+std::ostream &operator<< (std::ostream &os, const Weekday &day);
+Weekday operator++ (Weekday &day,int);
+Weekday operator-- (Weekday &day,int);
+
+
+std::ostream &operator<< (std::ostream &os, const Pressure &p);
+Pressure operator++ (Pressure &p);
---
+++ 6/6_1.cpp
@@ -0,0 +1,29 @@
+using namespace std;
+#inclue"6.h"
+const int num_days=7;
+const int num_pressure=4;
+int main(int argc, char * argv[]){
+    
+    
+    Pressure p= Pressure::lo;
+    Weekday w=Weekday::Monday;
+
+   // for(Weekday w:set1)cout<<w<<endl;
+    //for(Weekday w:set2)cout<<w<<endl;
+    /*
+    cout<<"lo<hi:"<<(p<p2)<<endl;
+    cout<<"Wednesday<Monday:"<<(w2<w1)<<endl;
+    /*
+    cout<<"Initial: "<<p<<endl;
+    cout<<"cout ++p: "<<++p<<endl;
+    */
+    for(int i=0;i<num_pressure;++i){
+        cout<<p;
+        ++p;
+    }
+    for(int i=0;i<num_days;++i){
+        cout<<w;
+        ++p;
+    }
+    return SUCCESS;
+}
