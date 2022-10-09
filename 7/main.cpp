4ac329229ed998ad2c36aeafaf1f2edee51cfe76
WashU-Yifan <wang.y1@wustl.edu>
2022-10-09 16:45:55 +0800

    "6"

--- 7/main.cpp
+++ 7/main.cpp
@@ -1,10 +1,14 @@
 
 #include<iostream>
+#include<memory>
 #include "derived.h"
 using std::cout;
 using std:: endl;
+using std::shared_ptr
+using std:: make_shared
 #define success 0
 int main(){
+    /*
     base b;
     derived d;
     base &br1=b;
@@ -24,12 +28,15 @@ int main(){
     cout<< "derived reference function call"<<endl;
     dr1.foo();
  */
-     cout<< "base pointer function call"<<endl;
-         bp1->foo();
-	     cout<< "base points to derived function call"<<endl;
-	         bp2->foo();
-		  cout<< "derived pointer function call"<<endl;
-		      dp->foo();
+    shared_ptr<base> sb1=make_shared<base>();
+    shared_ptr<base> sb2=make_shared<derived>();
+    shared_ptr<base> sd=make_shared<derived>();
+    cout<< "base shared pointer function call"<<endl;
+    sb1->foo();
+	cout<< "base points to derived function call"<<endl;
+	sb2->foo();
+	cout<< "derived shared pointer function call"<<endl;
+	sd->foo();
 
 
     return success;
