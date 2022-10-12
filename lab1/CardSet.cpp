1a5a5ed73163f4831b9f9b921a29d2e170595273
WashU-Yifan <wang.y1@wustl.edu>
2022-10-13 00:28:50 +0800

    "modify print"

--- lab1/CardSet.cpp
+++ lab1/CardSet.cpp
@@ -4,9 +4,10 @@ using std::ostream;
 using std::size_t;
 using std::cout;
 using std::endl;
+template <typename R, typename S>
 void CardSet::print(std::ostream& os,std::size_t size){
     size_t cnt=0;
-    for(auto card:cardset){
+    for(auto card:this->cardset){
         os<<card<<" ";
         if(++cnt==size){
             cnt=0;
