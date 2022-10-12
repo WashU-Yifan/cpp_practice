1b384044e1869d532886b5d4596d113f901f5146
WashU-Yifan <wang.y1@wustl.edu>
2022-10-12 16:27:18 +0800

    "4"

--- 9/9.cpp
+++ 9/9.cpp
@@ -1,10 +1,11 @@
 #include "9.h"
+#include<memory>
 #define success 0;
 size_t MyClass::NumObject;
 int main(){
 	//size_t MyClass::NumObject=0;
     	//MyClass O1,O2(O1);
-    MyClass *myarray=new MyClass[10];
-	delete myarray;
+	shared_ptr<MyClass> sp1=make_shared(),sp2=make_shared(&sp1);
+	
     return success;
 }
