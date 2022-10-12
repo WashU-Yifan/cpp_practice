1985a2b67499b24759f189386981a3f5d49daa1f
WashU-Yifan <wang.y1@wustl.edu>
2022-10-12 08:37:09 -0500

    done

--- 9/9.cpp
+++ 9/9.cpp
@@ -2,10 +2,13 @@
 #include<memory>
 #define success 0;
 size_t MyClass::NumObject;
+using std::shared_ptr;
+using std::make_shared;
+
 int main(){
 	//size_t MyClass::NumObject=0;
     	//MyClass O1,O2(O1);
-	shared_ptr<MyClass> sp1=make_shared(),sp2=make_shared(&sp1);
-	
+	shared_ptr<MyClass> sp1=make_shared<MyClass>(),sp2=make_shared<MyClass>(*sp1);
+	std::cout<<sp1.get()<<sp2.get()<<std::endl;	
     return success;
 }
