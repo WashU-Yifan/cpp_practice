9d7ee8b7851a5e6e58a8db3d070d10250b0cc415
WashU-Yifan <wang.y1@wustl.edu>
2022-10-12 09:10:23 -0500

    9.6

--- 9/9.cpp
+++ 9/9.cpp
@@ -5,22 +5,28 @@ size_t MyClass::NumObject;
 using std::shared_ptr;
 using std::make_shared;
 using std::weak_ptr;
-using std::lock;
 using std::cout;
 using std::endl;
 int main(){
 	//size_t MyClass::NumObject=0;
     	//MyClass O1,O2(O1);
+	
 	shared_ptr<MyClass> sp1=make_shared<MyClass>(),sp2=make_shared<MyClass>(*sp1);
-	std::cout<<sp1.get()<<sp2.get()<<std::endl;	
+	
 	weak_ptr<MyClass> wp1(sp1);
+	cout<<"assigning sp1=sp2"<<endl;
+		sp1=sp2;
+	cout<<"assignment done"<<endl;
 	shared_ptr<MyClass> sp3=wp1.lock();
 	if(sp3==nullptr){
 		cout<<"weak ptr wp1 is no longer pointing to a valid object"<<endl;
 	}
 	else{
+		cout<<sp1.use_count()<<endl
+			;
 		sp3->fun();
-		sp3.reset(nullptr);
+		sp3.reset();
+		cout<<sp1.use_count()<<endl;
 	}
     return success;
 }
