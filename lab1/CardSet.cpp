ec542e07ab5b3e423adc233d3ee8dee0c32685e9
WashU-Yifan <wang.y1@wustl.edu>
2022-10-13 02:00:52 +0800

    "9done"

--- lab1/CardSet.cpp
+++ lab1/CardSet.cpp
@@ -1,9 +1,11 @@
 #include "CardSet.h"
 #include<iostream>
+#include<stdexcept>
 using std::ostream;
 using std::size_t;
 using std::cout;
 using std::endl;
+
 template <typename R, typename S>
 void CardSet<R,S>::print(std::ostream& os,std::size_t size){
     size_t cnt=0;
@@ -14,4 +16,17 @@ void CardSet<R,S>::print(std::ostream& os,std::size_t size){
             os<<endl;
         }
     }
+}
+
+
+template<typenameR,typename S>
+CardSet<R,S>& CardSet<R,S>::operator>>(CardSet<R,S>& set2){
+    if(is_empty()){
+        throw std::runtime_error("current cardset is empty");
+    }
+    else{
+        set2.cardset.push_back(*(this->cardset.end()-1));
+        this->cardset.pop_back();
+    }
+    return *this;
 }
--- lab1/lab1.cpp
+++ lab1/lab1.cpp
@@ -7,7 +7,17 @@ int main(int , char**){
 
     Pinochle p=Pinochle();
     HoldEm T=HoldEm();
+    try{
+        while(1){
+            p>>T;
 
+        }
+
+    }
+    catch(runtime_error e){
+        cout<<e.what()<<endl;
+        
+    }
     p.print(cout,4);
     cout<<" print method1  done"<<endl;
     
