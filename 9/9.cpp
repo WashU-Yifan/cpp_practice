7c4a0f7edb19318d7ea7adfa37494b1d94877c9a
WashU-Yifan <wang.y1@wustl.edu>
2022-10-12 15:18:48 +0800

    "9.1"

---
+++ 9/9.cpp
@@ -0,0 +1,7 @@
+#include "9.h"
+#define success 0;
+int main(){
+    MyClass O1,O2(O1);
+    
+    return success;
+}
---
+++ 9/9.h
@@ -0,0 +1,16 @@
+#include<iostream>
+class MyClass{
+    public:
+        static size_t NumObject=0;
+        size_t ObjectID;
+        MyClass():ObjectID(NumObject++){
+            std::cout<<"Default constructor called OBJECTID: "<<ObjectID<<" ADDRESS: "<<this<<std::endl;
+        }
+        MyClass(const  MyClass& copy):ObjectID(copy.ObjectID){
+            ++NumObject;
+             std::cout<<"Copy constructor called OBJECTID: "<<ObjectID<<" ADDRESS: "<<this<<std::endl;
+        }
+        ~MyClass(){
+             std::cout<<"Destructor called OBJECTID: "<<ObjectID<<" ADDRESS: "<<this<<std::endl;
+        }
+};
