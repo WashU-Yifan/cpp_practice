73359100b5e3969be295749d08870f99265a0e7f
WashU-Yifan <wang.y1@wustl.edu>
2022-10-12 22:34:47 +0800

    "7"

--- 9/9.cpp
+++ 9/9.cpp
@@ -7,26 +7,21 @@ using std::make_shared;
 using std::weak_ptr;
 using std::cout;
 using std::endl;
+using std::unique_ptr;
+unique_ptr<MyClass> make_unique();
+void call_fun(unique_ptr<MyClass>& upt);
 int main(){
 	//size_t MyClass::NumObject=0;
     	//MyClass O1,O2(O1);
 	
-	shared_ptr<MyClass> sp1=make_shared<MyClass>(),sp2=make_shared<MyClass>(*sp1);
-	
-	weak_ptr<MyClass> wp1(sp1);
-	cout<<"assigning sp1=sp2"<<endl;
-		sp1=sp2;
-	cout<<"assignment done"<<endl;
-	shared_ptr<MyClass> sp3=wp1.lock();
-	if(sp3==nullptr){
-		cout<<"weak ptr wp1 is no longer pointing to a valid object"<<endl;
-	}
-	else{
-		cout<<sp1.use_count()<<endl
-			;
-		sp3->fun();
-		sp3.reset();
-		cout<<sp1.use_count()<<endl;
-	}
+	unique_ptr<Myclass> up1=make_unique();
+	call_fun(upt);
+	upt->fun();
     return success;
+}
+unique_ptr<MyClass> make_unique(){
+	return unique_ptr<MyClass>(new MyClass());
+}
+void call_fun(unique_ptr<MyClass>& upt){
+	upt->fun();
 }
