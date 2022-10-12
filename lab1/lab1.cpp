34396bb7d53dc5d5d04539cc5e32acc0697f0afd
WashU-Yifan <wang.y1@wustl.edu>
2022-10-13 02:06:29 +0800

    "done"

--- lab1/lab1.cpp
+++ lab1/lab1.cpp
@@ -7,10 +7,6 @@ int main(int , char**){
 
     Pinochle p=Pinochle();
     HoldEm T=HoldEm();
-<<<<<<< HEAD
-    p.shuffle();
-    T.shuffle();
-=======
     try{
         while(1){
             p>>T;
@@ -22,7 +18,6 @@ int main(int , char**){
         cout<<e.what()<<endl;
         
     }
->>>>>>> ec542e07ab5b3e423adc233d3ee8dee0c32685e9
     p.print(cout,4);
     cout<<" print method1  done"<<endl;
     
