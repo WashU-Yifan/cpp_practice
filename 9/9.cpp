de89e7c7cd2ba9ea19e0ac49285cc400cb9b1c92
WashU-Yifan <wang.y1@wustl.edu>
2022-10-12 02:39:39 -0500

    9.2

--- 9/9.cpp
+++ 9/9.cpp
@@ -1,7 +1,9 @@
 #include "9.h"
 #define success 0;
+size_t MyClass::NumObject;
 int main(){
-    MyClass O1,O2(O1);
+	//size_t MyClass::NumObject=0;
+    	MyClass O1,O2(O1);
     
     return success;
 }
--- 9/9.h
+++ 9/9.h
@@ -1,7 +1,7 @@
 #include<iostream>
 class MyClass{
     public:
-        static size_t NumObject=0;
+        static size_t NumObject;
         size_t ObjectID;
         MyClass():ObjectID(NumObject++){
             std::cout<<"Default constructor called OBJECTID: "<<ObjectID<<" ADDRESS: "<<this<<std::endl;
