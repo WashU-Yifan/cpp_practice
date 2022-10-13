fa891597383e0b749b0f77d055f5067c6688dbcb
WashU-Yifan <wang.y1@wustl.edu>
2022-10-14 01:20:54 +0800

    "fix random generator"

--- lab1/Deck.cpp
+++ lab1/Deck.cpp
@@ -2,7 +2,7 @@
 template<typename R, typename S>
 void Deck<R,S>::shuffle(){
     std::random_device rd;
-    std::mt19937 rn(rd);
+    std::mt19937 rn(rd());
     std::shuffle(CardSet<R,S>::cardset.begin(),CardSet<R,S>::cardset.end(),rn);
 }
 
