fc43826a38600a9e0e4aa2b291dcab63d2cc122b
WashU-Yifan <wang.y1@wustl.edu>
2022-10-27 17:56:27 +0800

    "12.3"

---
+++ 12/12.cpp
@@ -0,0 +1,11 @@
+#include"12.h"
+#include<iostream>
+
+using std::cout;
+using std::endl;
+#define SUCCESS 0
+int main(){
+    my_struct s1(1),s2(2);
+    cout<<"s1: "<<s1.i<<", s2: "<<s2.i<<endl;
+    return SUCCESS;
+}
