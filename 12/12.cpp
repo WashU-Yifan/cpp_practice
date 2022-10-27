14688bacb64b5608a17115772e7b830585c5d7b3
WashU-Yifan <wang.y1@wustl.edu>
2022-10-27 18:38:43 +0800

    "12.6"

--- 12/12.cpp
+++ 12/12.cpp
@@ -5,7 +5,7 @@ using std::cout;
 using std::endl;
 #define SUCCESS 0
 int main(){
-    my_struct s1(1),s2(2);
+    my_struct<> s1(1),s2(2);
     cout<<"s1: "<<s1.i<<", s2: "<<s2.i<<endl;
     std::swap(s1,s2);
     cout<<"s1: "<<s1.i<<", s2: "<<s2.i<<endl;
--- 12/12.h
+++ 12/12.h
@@ -1,3 +1,5 @@
+#include "12.cpp"
+template<typename T=int>
 struct my_struct{
     int i;
     my_struct(int _i):i(_i){}
@@ -5,3 +7,6 @@ struct my_struct{
    //my_struct& operator=(const my_struct&)=delete;
 //    ~my_struct()=delete;
 };
+#ifdef TEMPLATE_HEADERS_INCLUDE_SOURCE
+    #include"12.cpp"
+  #endif
