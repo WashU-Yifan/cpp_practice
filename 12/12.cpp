d9840f6e1405f3d42f3d3458f704e0b7dc987732
WashU-Yifan <wang.y1@wustl.edu>
2022-10-27 18:12:41 +0800

    "12.4"

--- 12/12.cpp
+++ 12/12.cpp
@@ -7,5 +7,7 @@ using std::endl;
 int main(){
     my_struct s1(1),s2(2);
     cout<<"s1: "<<s1.i<<", s2: "<<s2.i<<endl;
+    std::swap(s1,s2);
+    cout<<"s1: "<<s1.i<<", s2: "<<s2.i<<endl;
     return SUCCESS;
 }
