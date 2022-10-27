9b2107157f96b5c58ddc62ed8d70b3ca4a7557b3
WashU-Yifan <wang.y1@wustl.edu>
2022-10-27 04:58:15 -0500

    12.3done

--- 12/12.h
+++ 12/12.h
@@ -3,5 +3,5 @@ struct my_struct{
     my_struct(int _i):i(_i){}
     my_struct(const my_struct&)=delete;
     my_struct& operator=(const my_struct&)=delete;
-    ~my_struct()=delete;
+//    ~my_struct()=delete;
 };
