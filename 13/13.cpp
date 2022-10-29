3b93624b48d8eb15126f18cf605fc4b072d37125
Yifan Wang <68475921+WashU-Yifan@users.noreply.github.com>
2022-10-29 21:26:50 +0800

    "add extra main file"

--- 13/13.cpp
+++ 13/13.cpp
@@ -1,26 +1,10 @@
 
 #include "13.h"
-#include<memory>
 
-#define success 0;
 
-using std::shared_ptr;
-using std::make_shared;
-using std::weak_ptr;
+
 using std::cout;
 using std::endl;
-using std::unique_ptr;
-typedef unique_ptr<MyClass> upt;
-
-upt fun(upt up);
-int main(){
-    upt up(new MyClass("hello"));
-    upt up2(my_move(up));
-    //fun (up2);
-    up=fun(my_move(up2));
-    up->foo();
-    return success;
-}
 
 void MyClass::foo(){
          cout<<"foo"<<endl;
@@ -28,7 +12,3 @@ void MyClass::foo(){
             cout<<"string member: "<<my_string<<endl;
 }
 
-upt fun(upt up){
-    //up->foo();
-    return up;
-}
