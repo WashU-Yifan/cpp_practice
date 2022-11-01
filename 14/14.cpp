77083f1d0a264e06321234d931197f8d6c149752
WashU-Yifan <wang.y1@wustl.edu>
2022-11-01 23:58:43 +0800

    "14.4"

--- 14/14.cpp
+++ 14/14.cpp
@@ -4,7 +4,11 @@
     os<<c.s;
     return os;
  }
-
+/*
 bool my_class::operator<(const my_class&m)const {
     return this->s<m.s;
-}
+}*/
+
+ bool operator<(const my_class& m1,const my_class& m2) {
+     return m1.s<m2.s;
+ }
--- 14/14.h
+++ 14/14.h
@@ -6,10 +6,12 @@ using std::ostream;
 class my_class{
     std::string s;
     friend  ostream & operator<<(ostream&,const my_class&);
+    friend bool operator<(const my_class& ,const my_class&) ;
     public:
         my_class(const std::string& _s):s(_s){}
-        bool operator<(const my_class&)const ;
+        //bool operator<(const my_class&)const ;
         
 };
 
  ostream & operator<<(ostream&,const my_class&);
+ bool operator<(const my_class& ,const my_class&) ;
