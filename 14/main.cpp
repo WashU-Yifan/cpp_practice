00b72b77b64202e61dbb7e5d55ef1548505cf6f3
WashU-Yifan <wang.y1@wustl.edu>
2022-11-02 00:58:41 +0800

    "14.done"

--- 14/main.cpp
+++ 14/main.cpp
@@ -8,9 +8,14 @@ namespace my_namespace {
         os << "from reggular fun: " << m;
     }
 }
+void fun(ostream &os, const my_class &m) {
+        os << "from reggular fun: " << m;
+    }
+
 int main(){
     my_class m1("m1"),m2("m2");
     my_namespace::fun(cout,m1);
+    fun(cout,m1);
     fun(cout,m2);
     cout<<"result of comparing m1<m2: ";
     fun (cout,(m1<m2));
