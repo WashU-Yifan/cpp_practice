8cca4abc5459fdb17c81785d4c8cf41b761bc0c8
WashU-Yifan <wang.y1@wustl.edu>
2022-11-18 01:46:37 +0800

    "19,.2"

--- 19/main.cpp
+++ 19/main.cpp
@@ -7,11 +7,11 @@
 #include<list>
 
 using namespace::std;
-int main(int argc, const char* argv[]) {
+int main(int argc, const char* tmp[]) {
     set<string>s;
 
-    cout<<argv[0];
-    const char * tmp[3]={"123","123","1223"};
+
+    
 
     copy(tmp,tmp+sizeof(tmp)/sizeof(*tmp),inserter(s,s.end()));
     copy(s.begin(),s.end(),ostream_iterator<decltype(*s.begin())>(cout," "));
