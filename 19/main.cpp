ddf78db0940cd4949b92890c8606e576783dc10b
WashU-Yifan <wang.y1@wustl.edu>
2022-11-17 12:07:46 -0600

    19.3

--- 19/main.cpp
+++ 19/main.cpp
@@ -7,13 +7,13 @@
 #include<list>
 
 using namespace::std;
-int main(int argc, const char* tmp[]) {
-    set<string>s;
+int main(int argc,  char* tmp[]) {
+    set<char*>s;
 
 
     
 
     copy(tmp,tmp+argc,inserter(s,s.begin()));
-    copy(s.begin(),s.end(),ostream_iterator<decltype(*s.begin())>(cout," "));
+    copy(s.begin(),s.end(),ostream_iterator<char *>(cout," "));
     return 0;
 }
